60763f81e95bfdf008bfbd13374892f4bc4644ba
dankmuneesh4812 <101308182+dankmuneesh4812@users.noreply.github.com>
2022-03-10 10:14:42 +0530

    Add files via upload

---
+++ surprising_wluff_snicket1.ino
@@ -0,0 +1,19 @@
+// C++ code
+//
+void setup()
+{
+ pinMode(7, OUTPUT);
+ pinMode(9, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(7, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(7, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(9, HIGH);
+  delay(1500); // Wait for 1000 millisecond(s)
+  digitalWrite(9, LOW);
+  delay(1500); // Wait for 1000 millisecond(s)
+}
